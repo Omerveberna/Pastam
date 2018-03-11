171f8efc25ac1b7b1437a55b5b9c725f8aa06b9b
Omerveberna <omerveberna@gmail.com>
2018-03-11 10:01:59 +0300

    sipariş activity de kulanıcıya email(user) hoşgeldin çıkarttım

--- app/src/main/java/com/example/omerveberna/pastam/SiparisActivity.java
+++ app/src/main/java/com/example/omerveberna/pastam/SiparisActivity.java
@@ -2,12 +2,43 @@ package com.example.omerveberna.pastam;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SiparisActivity extends AppCompatActivity {
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseAuth auth;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_siparis);
+
+        text = (TextView) findViewById(R.id.text);
+
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("users/"+FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Users user = dataSnapshot.getValue(Users.class);
+                text.setText("Merhaba , " + user.getEmail() +"Hoşgeldin");
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
