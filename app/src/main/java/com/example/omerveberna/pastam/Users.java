7e991a05fd8e67fd4b63945d67cb41d8ba09d1da
Omerveberna <omerveberna@gmail.com>
2018-03-11 10:33:13 +0300

    sipariş activity de kulanıcıya email(user) hoşgeldin çıkarttım hata duzeltme

---
+++ app/src/main/java/com/example/omerveberna/pastam/Users.java
@@ -0,0 +1,25 @@
+package com.example.omerveberna.pastam;
+
+/**
+ * Created by omerveberna on 11.03.2018.
+ */
+
+public class Users {
+    private String email;
+
+    public Users(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+
+        this.email = email;
+    }
+    public Users() {
+    }
+
+}
